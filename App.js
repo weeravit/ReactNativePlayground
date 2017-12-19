a23bf1b418cc2e5e1cf018e4ed0c9d06851731db
Weeravit Sirita <weeravit.sirita@gmail.com>
2017-12-19 15:46:41 +0700

    add react navigation

--- App.js
+++ App.js
@@ -5,41 +5,51 @@
  */
 
 import React, {Component} from 'react';
-import {
-    Platform,
-    StyleSheet,
-    Text as RNText
-} from 'react-native';
+import {Platform, StyleSheet, View} from 'react-native';
 
 import {Button, Container, Content, Header, Text} from 'native-base';
 
 export default class App extends Component<{}> {
     render() {
         return (
-            <Container>
-                <Header/>
-                <Content>
-                    <Button rounded>
+            <View style={styles.containerStyles}>
+                <View style={styles.contentStyles}>
+                    <View style={styles.itemStyles}/>
+
+                    <Button rounded style={styles.buttonStyles}>
                         <Text style={styles.textStyles}>Click Me!</Text>
                     </Button>
-                </Content>
-            </Container>
+                </View>
+
+                <View style={styles.bottomBarStyles}/>
+            </View>
         );
     }
 }
 
-const platformColor = Platform.select({
-    ios: {
-        color: '#fffd1a'
-    },
-    android: {
-        color: '#ff00ff'
-    }
-})
-
 const styles = StyleSheet.create({
+    containerStyles: {
+        flex: 1
+    },
+    buttonStyles: {
+        alignSelf: 'center'
+    },
+    itemStyles: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#ff0e16'
+    },
     textStyles: {
-        // color: Platform.OS === 'ios' ? '#fffd1a' : '#ff00ff'
-        ...platformColor
+        color: Platform.OS === 'ios' ? '#fffd1a' : '#ff00ff'
+    },
+    contentStyles: {
+        flex: 0.9,
+        backgroundColor: '#ff00ff',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    bottomBarStyles: {
+        flex: 0.1,
+        backgroundColor: '#0f0fff'
     }
 });
